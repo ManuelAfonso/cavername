4834875c599988bdf1ca8c7eca24729cd7a81ec9
ManuelAfonso <mgafonso@gmail.com>
2015-03-26 13:49:00 +0000

    Create options.php

---
+++ design/bts1-orange/options.php
@@ -0,0 +1,11 @@
+<?php
+$optionsNavigation['navigation--blocks-navigation2']['navbar'] = 'navbar-inverse'; // pode-se acrescentar navbar-fixed-top
+$optionsNavigation['navigation--blocks-navigation2']['align'] = 'navbar-right'; 
+$optionsNavigation['navigation--blocks-navigation2']['collapse'] = 1;
+$optionsNavigation['navigation--blocks-navigation2']['brand'] = 'visible-xs'; // opções para navbar-brand
+
+$optionsNavigation['mainzone--blocks-navigation2']['navbar'] = 'navbar-default'; // pode ser navbar-inverse; pode-se acrescentar navbar-fixed-top
+$optionsNavigation['mainzone--blocks-navigation2']['align'] = ''; // pode ser navbar-right
+$optionsNavigation['mainzone--blocks-navigation2']['collapse'] = 0;
+$optionsNavigation['mainzone--blocks-navigation2']['brand'] = ''; // opções para navbar-brand
+?>
