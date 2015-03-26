ef9a3ee3fdd9e9ee0d4c5b318914bba324f4eab7
ManuelAfonso <mgafonso@gmail.com>
2015-03-26 16:40:53 +0000

    Create common.php fixes #32

---
+++ design/bts1-orange/common.php
@@ -0,0 +1,5 @@
+<?php
+$bootstrapCss = "http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css";
+$bootstrapJs = "http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js";
+$jqueryJs = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js";
+?>
